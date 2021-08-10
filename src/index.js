20b29c5dd1c1ce1683bb0e742144149d6905498d
Jerrison <jerryyip24@gmail.com>
2021-08-10 10:30:00 -0400

    need to fix even/odd innerContainer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { inboxActive, todayActive, projectsActive, homeActive } from './active.js'
-import { newTask  } from './newTask.js';
+import { createTaskObject, generateValues  } from './newTask.js';
 
 const menuIcon = document.querySelector('.fa-bars');
 const homeIcon = document.querySelector('.fa-home');
@@ -39,11 +39,58 @@ projects.addEventListener('click', () => {
 })
 
 
-// Add Task button Event Listener
+// Add New Task button Event Listener
 const addTask = document.querySelectorAll('.add-task');
+const modal = document.querySelector('.modal-container');
 
 addTask.forEach(task => {
     task.addEventListener('click', () => {
-        newTask();
+        modal.classList.remove('inactive')
+        // newTask();
     })
 })
+
+
+// Generating values and printing new task ALSO deletes task
+const addTaskButton = document.querySelector('.modal-add');
+let deleteTask;
+let newTaskContainer;
+
+addTaskButton.addEventListener('click', () => {
+    createTaskObject()
+    // have to redefine trash everytime you create a new task to keep the array updated
+    deleteTask = document.querySelectorAll('.fa-trash-alt');
+
+    // event listener for each trash Icon, e.target targets only the one selected
+    deleteTask.forEach(task => {
+        task.addEventListener('click', (e) => {
+           e.target.parentNode.parentNode.remove()
+        })
+    })
+
+    // event listener to hide/show innerContainer (task details)
+    newTaskContainer = document.querySelectorAll('.newTaskContainer');
+
+    newTaskContainer.forEach(task => {
+        task.addEventListener('click', (e) => {
+            if (e.target.parentNode.parentNode.classList.contains('newTaskContainer') && e.target.firstChild !== null) {
+                e.target.parentNode.parentNode.children[2].classList.toggle('inactive');
+            }
+        })
+    })
+}) 
+
+
+// Add Task - Close event listeners
+const modalX = document.querySelector('.fa-times');
+const modalClose = document.querySelector('.modal-close');
+
+[modalX, modalClose].forEach(task => {
+    task.addEventListener('click', () => {
+        modal.classList.add('inactive')
+    })
+})
+
+
+// questions for Shiranka - How to append an element in Inbox and another location as well
+// how to get innerDescription to toggleactive
--- src/newTask.js
+++ src/newTask.js
@@ -6,17 +6,114 @@ const inboxTaskContainer = document.querySelector('.inbox-task-container');
 const todayTaskContainer = document.querySelector('.today-task-container');
 const projectsTaskContainer = document.querySelector('.projects-task-container');
 
+let newValues;
 
-let newTask = () => {
-    if (!inboxContainer.classList.contains('inactive')) {
-        let newTask = document.createElement('span');
-        let checkBox = document.createElement('input');
-        checkBox.setAttribute('type','checkbox')
-        
-        newTask.append(checkBox);
-        inboxTaskContainer.append(newTask);
 
+let generateValues = (title, description, date, priority) => {
+    return {
+        title,
+        description,
+        date,
+        priority,
     }
 }
 
-export { newTask };
+let createTaskObject = () => {
+
+    // generate the task that is seen on default view
+    let titleValue = document.querySelector('.title').value;
+    let descriptionValue = document.querySelector('.description').value;
+    let dateValue = document.querySelector('.date').value;
+    let priorityValue = document.querySelector('#priority').value;
+
+    newValues = generateValues(titleValue, descriptionValue, dateValue, priorityValue);
+
+    let newTaskContainer = document.createElement('div');
+    let firstContainer = document.createElement('div');
+    let secondContainer = document.createElement('div');
+    let checkBox = document.createElement('input');
+    checkBox.setAttribute('type','checkbox');
+    let title = document.createElement('p');
+    let date = document.createElement('p');
+    let priority = document.createElement('i');
+    let trash = document.createElement('i');
+    
+    newTaskContainer.classList.add('newTaskContainer');
+    firstContainer.classList.add('firstContainer');
+    secondContainer.classList.add('secondContainer');
+    trash.classList.add('far', 'fa-trash-alt');
+
+    if (priorityValue === 'low') {
+        priority.classList.add('fas', 'fa-flag', 'flag-low');
+    } else if (priorityValue === 'medium') {
+        priority.classList.add('fas', 'fa-flag', 'flag-medium');
+    } else {
+        priority.classList.add('fas', 'fa-flag', 'flag-high');
+    }
+
+    title.textContent = newValues.title;
+    date.textContent = newValues.date;
+
+    secondContainer.append(date, priority, trash);
+    firstContainer.append(checkBox, title);
+
+    // generate the in-depth view of task
+    let innerContainer = document.createElement('div');
+    let innerFirst = document.createElement('div');
+    let innerSecond = document.createElement('div');
+    let innerTitle = document.createElement('span');
+    let innerDate = document.createElement('span');
+    let innerDescription = document.createElement('span');
+    let innerPriority = document.createElement('span');
+    let innerTitleS = document.createElement('span');
+    let innerDateS = document.createElement('span');
+    let innerDescriptionS = document.createElement('span');
+    let innerPriorityS = document.createElement('span');
+
+    innerContainer.classList.add('innerContainer', 'inactive');
+    innerFirst.classList.add('innerFirst');
+    innerSecond.classList.add('innerSecond');
+
+    innerTitleS.classList.add('strong');
+    innerDateS.classList.add('strong');
+    innerDescriptionS.classList.add('strong');
+    innerPriorityS.classList.add('strong');
+    innerTitle.classList.add('regular');
+    innerDate.classList.add('regular');
+    innerDescription.classList.add('regular');
+    innerPriority.classList.add('regular');
+
+    innerTitleS.textContent = 'Title:';
+    innerDateS.textContent = 'Due Date:';
+    innerDescriptionS.textContent = 'Description:';
+    innerPriorityS.textContent = 'Priority:';
+    innerTitle.textContent = newValues.title;
+    innerDate.textContent = newValues.date;
+    innerDescription.textContent = newValues.description;
+    innerPriority.textContent = newValues.priority;
+
+
+    innerTitleS.append(innerTitle)
+    innerDateS.append(innerDate)
+    innerDescriptionS.append(innerDescription)
+    innerPriorityS.append(innerPriority)
+
+    innerFirst.append(innerTitleS, innerDateS);
+    innerSecond.append(innerDescriptionS, innerPriorityS);
+    innerContainer.append(innerFirst, innerSecond);
+
+    newTaskContainer.append(firstContainer, secondContainer, innerContainer);
+
+    // checks to see which tab is active, and appends to it
+    if (!inboxContainer.classList.contains('inactive')) {
+        inboxTaskContainer.append(newTaskContainer);
+    } else if (!todayContainer.classList.contains('inactive')) {
+        todayTaskContainer.append(newTaskContainer);
+    } else {
+        projectsTaskContainer.append(newTaskContainer);
+    }
+}
+
+
+
+export { createTaskObject, generateValues };
